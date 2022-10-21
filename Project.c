9905bd44c843103d62332c97e265a5cf7500472a
Mine04D-ux <115807934+Mine04D-ux@users.noreply.github.com>
2022-10-21 16:24:54 +0100

    Update Project.c

--- Project.c
+++ Project.c
@@ -4,9 +4,14 @@
 int main()
 {
     char name[30];
-    printf("Enter name: ");
+    float saldo_cp, tanb, saldo_est;
+    printf("Insira o nome da sua conta poupança: ");
     gets(name);     //Function to read string from user.
-    printf("Name: ");
-    puts(name);    //Function to display string.
+    printf("Insira o saldo da sua conta poupança: ");
+    scanf("%f", saldo_cp);
+    printf("Insira o tanb da sua conta poupança: ");
+    scanf("%f", tnb);
+    saldo_est = saldo_cp + (saldo_cp * tanb);
+    printf("A sua conta %s tera o saldo no final do ano de %f", name, saldo_est);
     return 0;
 }
